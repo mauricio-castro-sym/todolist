d7b11a1c17deac87a849a40a78ed48b58c05941d
Mauricio Castro <mauricio.castro@symplifica.com>
2022-08-24 07:56:06 -0500

    all in one

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :tasks
+  post 'tasks/:id/active' => 'tasks#active', as: :complete_task
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
